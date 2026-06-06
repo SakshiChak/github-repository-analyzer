7bc34a8909c6d0c3608f00bdfd737fa5af5bec7b
SakshiChak <mailme.sakshichak@gmail.com>
2026-06-06 16:47:14 +0530

    add root route for backend health check

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("GitHub Repo Analyzer API is running 🚀");
+});
+
 app.use("/api/github", githubRoutes);
 
 export default app;
